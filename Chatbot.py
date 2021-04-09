3e6419a8bf3bad6c3e3be53963d98f0abc6f1872
NoahWood01 <noahdanielwood@gmail.com>
2021-04-09 15:59:32 -0500

    Working AI-Chatbot
    Although the responses and available questioning is low, the program works as intended. Right now the probability that it responds must have a strength of more than .9, may change later.

--- Chatbot.py
+++ Chatbot.py
@@ -2,58 +2,120 @@ import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-import numpy as np
+import numpy
 import tflearn
 import tensorflow
+from tensorflow.python.framework import ops
 import random
 import json
-print("\n\n")
+import pickle
 
 with open("intents.json") as file:
     data = json.load(file)
 
-words = []
-labels = []
-docs_x = []
-docs_y = []
+#when retraining might need to alter this code so it doesnt load the old one
+try:
+    with open("data.pickle","rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
 
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        wrds = nltk.word_tokenize(pattern)
-        words.extend(wrds)
-        docs_x.append(pattern)
-        docs_y.append(intent["tag"])
+    #putting things into lists
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
 
-        if intent["tag"] not in labels:
-            labels.append(intent["tag"])
+            if intent["tag"] not in labels:
+                labels.append(intent["tag"])
 
-words = [stemmer.stem(w.lower()) for w in words]
-words = sorted(list(set(words)))
+    words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
+    words = sorted(list(set(words)))
 
-labels = sorted(labels)
+    labels = sorted(labels)
 
-#tracking frequency of words in terms of 1 and 0
+    #tracking frequency of words in terms of 1 and 0
+    training = []
+    output = []
 
-training = []
-output = []
+    out_empty = [0 for _ in range(len(labels))]
 
-out_empty = [0 for _ in range(len(labels))]
+    for x, doc in enumerate(docs_x):
+        bag = []
 
-for x, doc in enumerate(docs_x):
-    bag = []
+        wrds = [stemmer.stem(w) for w in doc]
 
-    wrds = [stemmer.stem(w) for w in doc]
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
 
-    for w in words:
-        if w in wrds:
-            bag.append(1)
+        training.append(bag)
+        output.append(output_row)
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+
+    with open("data.pickle","wb") as f:
+        pickle.dump((words, labels, training, output), f)
+
+ops.reset_default_graph()
+
+#Neural Networking and learning
+net = tflearn.input_data(shape=[None, len(training[0])])
+#add more .fully_connected for more data
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+try:
+    model.load("model.tflearn")
+except:
+    #n_epoch is now much the machine learns (1000 times)
+    model.fit(training, output, n_epoch=1000, batch1_size=8, show_metric=True)
+    model.save("model.tflearn")
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    return numpy.array(bag)
+
+def chat():
+    print("Start talking with the bot! (type quit to stop)")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+
+        results = model.predict([bag_of_words(inp,words)])[0]
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+
+        if results[results_index] > 0.90:
+            for tg in data["intents"]:
+                if tg['tag'] == tag:
+                    responses = tg['responses']
+
+            print(random.choice(responses))
         else:
-            bag.append(0)
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
+            print("I don't quite understand. Can you ask again or try another question?")
 
-    training.append(bag)
-    output.append(output_row)
-
-training = np.array(training)
-output = np.array(output)
+chat()
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # AI-Chatbot-Python
  
+Step 1:
+Create training so that the Chatbot can learn from existing sentences/text.
+    Use tflearn with varying interations to learn from the given .json file.
+
+Step 2:
+use the training in order to have the Chatbot respond with high probability.
+    for right now a lot of topics will not have the "correct" response.
+
+Step 3:
+Create a GUI for the Chatbot so that it works outside of the command prompt/IDE.
+
+Step 4:
+Figure out a way to supply more text to the training so that the Chatbot can respond and correctly recognize more question and text.
